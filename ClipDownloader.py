32b43c9635e6343302f9eb2a449897ec2097d605
Zaid Omer <zaidomerone@gmail.com>
2020-07-11 01:22:53 -0400

    Preliminary Files

---
+++ ClipDownloader.py
@@ -0,0 +1,5 @@
+def downlaodTwitchClips:
+    twitchURL = "https://www.twitch.tv/directory/game/VALORANT/clips?range=24hr"
+
+def downloadRedditClips:
+    redditURL = "https://www.reddit.com/r/ValorantClips/"
