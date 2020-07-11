42f3262fcf368c9b9ac73315c950a4337995d04d
Zaid Omer <zaidomerone@gmail.com>
2020-07-11 01:25:44 -0400

    Class declared

--- ClipDownloader.py
+++ ClipDownloader.py
@@ -1,5 +1,6 @@
-def downlaodTwitchClips:
-    twitchURL = "https://www.twitch.tv/directory/game/VALORANT/clips?range=24hr"
+class ClipDownloader:
+    def downlaodTwitchClips:
+        twitchURL = "https://www.twitch.tv/directory/game/VALORANT/clips?range=24hr"
 
-def downloadRedditClips:
-    redditURL = "https://www.reddit.com/r/ValorantClips/"
+    def downloadRedditClips:
+        redditURL = "https://www.reddit.com/r/ValorantClips/"
